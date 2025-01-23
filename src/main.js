c5925f44b8c18632f0d12d952824bf76b8293d31
Mihail <mykh.shev4enko@gmail.com>
2025-01-23 20:32:56 +0200

    Done

---
+++ src/main.js
@@ -0,0 +1,71 @@
+import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
+import 'izitoast/dist/css/iziToast.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { createGalleryListTemplate } from './js/render-functions';
+import { searchPhotoApi } from './js/pixabay-api';
+import { showLoader } from './js/render-functions.js';
+import { hideLoader } from './js/render-functions.js';
+
+const form = document.querySelector('.main-form');
+const list = document.querySelector('.list-gallery');
+const allert = iziToast;
+let simplelightbox;
+
+const searchUserPhotoApi = e => {
+  e.preventDefault();
+  showLoader();
+
+  const userSearchQuery =
+    e.currentTarget.elements.user_search_query.value.trim();
+
+  searchPhotoApi(userSearchQuery)
+    .then(data => {
+      if (userSearchQuery === '') {
+        allert.show({
+          title: '❌',
+          color: '#d1c542',
+          position: 'topRight',
+          message: 'Please enter a keyword',
+        });
+
+        form.reset();
+        list.innerHTML = '';
+        return;
+      }
+
+      if (data.total === 0) {
+        allert.show({
+          title: '❌ Sorry',
+          color: 'red',
+          position: 'topRight',
+          message:
+            'There are no images matching your search query. Please try again!',
+        });
+
+        form.reset();
+        list.innerHTML = '';
+        return;
+      }
+
+      const galleryTemplate = data.hits
+        .map(el => createGalleryListTemplate(el))
+        .join('');
+
+      list.innerHTML = galleryTemplate;
+      simplelightbox = new SimpleLightbox('.list-gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      simplelightbox.refresh();
+      form.reset();
+    })
+
+    .catch(err => {
+      console.log(err);
+    })
+    .finally(hideLoader());
+};
+
+form.addEventListener('submit', searchUserPhotoApi);
